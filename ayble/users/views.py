bf0a3fb7674657bf70f218e5edcba58b3da89753
discotechie <sue.e.aman@gmail.com>
2023-02-02 14:44:38 -0500

    added an extra perm check

--- ayble/users/views.py
+++ ayble/users/views.py
@@ -6,9 +6,18 @@ from rest_framework.permissions import IsAuthenticated
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     queryset = CustomUser.objects.all()
-    # overriding this method so that list view is more performant 
-    # and retrieve has more detail about the user
+
+    def get_queryset(self):
+        user = self.request.user
+        # only show the response if requesting user = user requested
+        # or if the user is superuser
+        if self.action == 'retrieve' and not user.is_superuser:
+            return CustomUser.objects.filter(email=user)
+        return CustomUser.objects.all()
+
+    # list should have smaller response
+    # retrieve has more detail about the user
     def get_serializer_class(self):
-        if self.action == 'list':
-            return UserList
-        return UserDetail
+        if self.action == 'retrieve':
+            return UserDetail
+        return UserList
